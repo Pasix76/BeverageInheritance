46d31d33219cdd26a5b34702e0d699cb44a1c59e
Pasix76 <123998943+Pasix76@users.noreply.github.com>
2025-08-13 15:09:15 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+  public static void main (String[] args) {
+    
+  }
+  
+  public static abstract class Beverage {
+    protected int currentML;
+    protected final int maxCapacity;
+    protected abstract String getML();
+    protected abstract String getName();
+    
+    public Beverage() {
+      maxCapacity = 500;
+      currentML = 500;
+    }
+    
+    public Beverage(int maxCapacity) {
+      this.maxCapacity = maxCapacity;
+      currentML = maxCapacity;
+    }
+    
+    public void drink() {}
+    
+    public void drink(int ml) {}
+    
+    public void refill() {}
+    
+    public void refill(int ml) {}
+    
+    public int getCurrentML() {
+      return currentML;
+    }
+    
+    public int getMaxCapacity() {
+      return maxCapacity;
+    }
+  }
+}
